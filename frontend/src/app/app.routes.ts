be804cf0838051a51390df856460c8552abb0660
dnisko <dnisko@hotmail.com>
2025-07-17 23:01:15 +0200

    added styling

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -3,10 +3,17 @@ import { HomeComponent } from './components/pages/home/home.component';
 import { CategoryListComponent } from './components/pages/category-list/category-list.component';
 import { RecipeListComponent } from './components/pages/recipe-list/recipe-list.component';
 import { SearchComponent } from './components/pages/search/search.component';
+import { LayoutComponent } from './components/layout/layout.component';
 
 export const routes: Routes = [
-    { path: '', component: HomeComponent },
-    { path: 'categories', component: CategoryListComponent },
-    { path: 'recipes', component: RecipeListComponent },
-    { path: 'search', component: SearchComponent }
+    {
+        path: '',
+        component: LayoutComponent,
+        children: [
+            { path: '', component: HomeComponent },
+            { path: 'categories', component: CategoryListComponent },
+            { path: 'recipes', component: RecipeListComponent },
+            { path: 'search', component: SearchComponent }
+        ],
+    },
 ];
--- frontend/src/app/components/layout/layout.component.ts
+++ frontend/src/app/components/layout/layout.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-layout',
-  imports: [],
+  standalone: true,
+  imports: [
+    RouterOutlet,
+    RouterLinkActive,
+    RouterLink
+  ],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.scss'
 })
-export class Layout {
+export class LayoutComponent {
 
 }
